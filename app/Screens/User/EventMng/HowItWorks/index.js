dbe0fbe125d82cd626233c084149c94776f5923c
Harsh sharma <harsh@techalchemy.dev>
2023-04-25 11:10:44 +0530

    add route for how i works

---
+++ app/Screens/User/EventMng/HowItWorks/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import HowItWorksView from "./components/HowItWorksView";
+
+const HowItWorks = () => {
+  return (
+    <HowItWorksView/>
+  );
+};
+
+export default HowItWorks;
